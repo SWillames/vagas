ad45cb012dff4201349a4f311b5ead3fd0b7412f
Sergio Costa <sergiowoc@gmail.com>
2024-01-15 15:53:51 -0300

    inserting validation and formatting the message.

---
+++ src/main/java/com/sw_software/vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,18 @@
+package com.sw_software.vagas.modules.candidate.controllers;
+
+import com.sw_software.vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+  @PostMapping("/")
+  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    System.out.println("Candidate: " + candidateEntity.getName());
+  }
+}
