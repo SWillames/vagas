2f125e87c538bbe2d69e1c353e5e6ce452bf0af3
Sergio Costa <sergiowoc@gmail.com>
2024-01-15 21:32:51 -0300

    refactoring the controller.

--- src/main/java/com/sw_software/vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/sw_software/vagas/modules/candidate/controllers/CandidateController.java
@@ -3,8 +3,10 @@ package com.sw_software.vagas.modules.candidate.controllers;
 import com.sw_software.vagas.exceptions.UserFoundException;
 import com.sw_software.vagas.modules.candidate.CandidateEntity;
 import com.sw_software.vagas.modules.candidate.repository.CandidateRepository;
+import com.sw_software.vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
   @Autowired
-  private CandidateRepository candidateRepository;
-
+  private CreateCandidateUseCase createCandidateUseCase;
   @PostMapping("/")
-  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-    this.candidateRepository
-        .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-        .ifPresent((user) -> {
-          throw new UserFoundException();
-        });
+  public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    try {
+      var result  = this.createCandidateUseCase.execute(candidateEntity);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
   }
 }
